fcc4c37006abb7c92b9f1613cf46494cb2389287
Nikita Grinyov <nikita3grynov@gmail.com>
2025-10-22 12:25:26 +0300

    comments

--- app/main.py
+++ app/main.py
@@ -1,21 +1,26 @@
 def format_linter_error(error: dict) -> dict:
-    _dict = {**{new: error[old] for old, new in
-        {
-            "line_number": "line",
-            "column_number": "column",
-            "text": "message",
-            "code": "name"
-        }.items() if old in error},
-        "source": "flake8"
+    _dict = {
+        **{
+            new: error[old]
+            for old, new in {
+                "line_number": "line",
+                "column_number": "column",
+                "text": "message",
+                "code": "name",
+            }.items()
+            if old in error
+        },
+        "source": "flake8",
     }
     return _dict
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    _dict = {"errors": [format_linter_error(e) for e in errors],
-             "path": file_path,
-             "status": "failed" if errors else "passed"
-            }
+    _dict = {
+        "errors": [format_linter_error(e) for e in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed",
+    }
     return _dict
 
 
